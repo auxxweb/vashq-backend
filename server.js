a12ad790f263c0a66168bb0fc62909979942c454
auxxweb <auxxweb@gmail.com>
2026-02-16 12:19:18 +0530

    second-cors-added commit

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'http://localhost:5173'],
+  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'http://localhost:5173', 'https://vashq.com'],
   credentials: true
 }));
 app.use(express.json());
